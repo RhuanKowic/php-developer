fb96c852185149e4d8a04e845312759f9402f746
Rhuan Kowic Santos <rhuanksantos2@gmail.com>
2024-06-19 10:06:52 -0300

    if e else.

---
+++ sintaxe/if.php
@@ -0,0 +1,5 @@
+<?php 
+    if (!true == false){
+        echo "comparação de igualdade";
+    }
+?>
---
+++ sintaxe/if_else.php
@@ -0,0 +1,10 @@
+<?php
+    $var = 8000;
+    $var2 = 10;
+
+    if ($var < $var2){
+        echo "var é menor que var2";
+    } else {
+        echo "var é maior que var2";
+    }
+?>
