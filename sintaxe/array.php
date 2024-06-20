0ed69b42a26fd1aa15b328527c9c64cc939bb893
Rhuan Kowic Santos <rhuanksantos2@gmail.com>
2024-06-20 10:03:28 -0300

    relembrado array.

--- sintaxe/array.php
+++ sintaxe/array.php
@@ -1,4 +1,11 @@
 <?php
     $cursos = array("PHP", "HTML", "CSS", "Kotlin", "Java");
     print_r($cursos);
+
+    $array = ["chave" => "valor"];
+    
+    $array2 = [1,2,3];
+    print_r($array["chave"]);
+    print "\n";
+    print_r($array2);
 ?>
