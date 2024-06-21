ce706a78cf1acf8baa8e96e3bbb8e213a6f5b549
Rhuan Kowic Santos <rhuanksantos2@gmail.com>
2024-06-21 09:37:21 -0300

    Instalando dependencia mpdf.

--- gerenciamento_pacotes/vendor/composer/autoload_real.php
+++ gerenciamento_pacotes/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitcd7ef05a25bc8e96a24404b0a5015056
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitcd7ef05a25bc8e96a24404b0a5015056', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitcd7ef05a25bc8e96a24404b0a5015056', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInitcd7ef05a25bc8e96a24404b0a5015056
         call_user_func(\Composer\Autoload\ComposerStaticInitcd7ef05a25bc8e96a24404b0a5015056::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitcd7ef05a25bc8e96a24404b0a5015056::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
